521650fb62e1f92dbd6dc3513c83dd4312dab5cb
Jonathan Crecencio <jonathancrecencio@outlook.com>
2021-01-26 00:07:31 -0300

    teste

---
+++ public/js/scripts.js
@@ -0,0 +1,16 @@
+let el = document.querySelector('.active')
+el.addEventListener('click', () => {
+    // Queremos mostrar o menu
+    let menuItems = document.querySelector('.menu_mobile ul')
+
+    // verifica se a classe existe, se existir ele remove a classe para mostrar e adiciona a classe para esconder
+    if(menuItems.classList.contains('show')){
+        menuItems.classList.add('hide')
+        menuItems.classList.remove('show')
+    }
+    // Se a classe não existir, ele adiciona a classe para mostrar e remove a classe "oculta"
+    else{
+        menuItems.classList.add('show')
+        menuItems.classList.remove('hide')
+    }
+})
